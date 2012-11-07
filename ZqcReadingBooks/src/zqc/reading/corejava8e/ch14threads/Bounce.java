454d18dd0422a0a905edfb7fafb56293c85b86dd
zqc_xubuntu <zhangqiancheng@itany.com>
2012-11-07 10:35:06 +0800

    Refactoring class ball intto its own file.

--- ZqcReadingBooks/src/zqc/reading/corejava8e/ch14threads/Bounce.java
+++ ZqcReadingBooks/src/zqc/reading/corejava8e/ch14threads/Bounce.java
@@ -7,8 +7,6 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
@@ -92,40 +90,6 @@ class BounceFrame extends JFrame {
     public static final int DEFAULT_HEIGHT = 350;
     public static final int STEPS = 1000;
     public static final int DELAY = 3;
-}
-
-class Ball{
-    public void move(Rectangle2D bounds){
-        x+= dx;
-        y += dy;
-        if (x < bounds.getMinX()){
-            x = bounds.getMinX();
-            dx = -dx;
-        }
-        if (x + XSIZE >= bounds.getMaxX()){
-            x = bounds.getMaxX() - XSIZE;
-            dx = -dx;
-        }
-        if (y < bounds.getMinY()){
-            y = bounds.getMinY();
-            dy= -dy;
-        }
-        if (y + YSIZE >= bounds.getMaxY()){
-            y = bounds.getMaxY() - YSIZE;
-            dy = -dy;
-        }
-    }
-    
-    public Ellipse2D getShape(){
-        return new Ellipse2D.Double(x,  y, XSIZE, YSIZE);
-    }
-    
-    private static final int XSIZE = 15;
-    private static final int YSIZE = 15;
-    private double x = 0;
-    private double y = 0;
-    private double dx = 1;
-    private double dy = 1;
 }
 
 class BallComponent extends JPanel
